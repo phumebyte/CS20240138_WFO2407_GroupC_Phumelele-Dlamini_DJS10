be48115f1192ebbb4b99f32e4a35a22dda29d1da
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-24 10:05:56 +0200

    Wrote fetch function using useEffect and destructured import statement

--- CS20240138_WFO2407_GroupC_Phumelele-Dlamini_DJS10/src/App.jsx
+++ CS20240138_WFO2407_GroupC_Phumelele-Dlamini_DJS10/src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react"
+import { useState , useEffect} from "react"
 
 function App() {
   
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+   fetch( 'https://jsonplaceholder.typicode.com/posts')
+   .then(response => {
+    if (!response.ok) {
+      throw new Error('');
+    }
+    return response.json();
+  })
+  .then(data => setPosts(data))
+  .catch(error => setError(error.message));
+  }, []);
+
   return (
     <>
       <h1>The blog goes here</h1>
